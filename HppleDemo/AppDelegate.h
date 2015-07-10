87f1005c22f02bfb6384ae6e472ec6c3fd631285
R0CKSTAR <yeahdongcn@gmail.com>
2015-07-10 10:26:20 +0800

    yifone网

---
+++ HppleDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HppleDemo
+//
+//  Created by R0CKSTAR on 15/7/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- HppleDemo/main.m
+++ HppleDemo/main.m
@@ -7,50 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "TFHpple.h"
-#import "AFNetworking.h"
-#import "Base64.h"
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        NSLog(@"\n\n\n");
-        NSMutableString *log = [NSMutableString new];
-        for (int cid = 1; cid < 2; cid++) {
-            for (int i = 1; i < 90; i++) {
-                NSData  *data     = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://aihuishou.com/product/search?cid=%d&bid=0&keyword=&pageIndex=%d", cid, i]]];
-                
-                TFHpple *doc      = [[TFHpple alloc] initWithHTMLData:data];
-                NSArray *elements = [doc searchWithXPathQuery:@"//ul[@class='products']/li/a"];
-                for (int j = 0; j < [elements count]; j++) {
-                    TFHppleElement *e = [elements objectAtIndex:j];
-                    NSString *href    = [e objectForKey:@"href"];
-                    NSString *base64name = @"";
-                    for (TFHppleElement *t in e.children) {
-                        if ([[t tagName] isEqualToString:@"div"]) {
-                            base64name = [[t text] base64EncodedString];
-                        }
-                    }
-                    
-                    base64name = [base64name stringByReplacingOccurrencesOfString:@"/" withString:@""];
-                    base64name = [base64name stringByReplacingOccurrencesOfString:@"\\" withString:@""];
-                    [log appendFormat:@"%@;", base64name];
-                    
-                    // Get possible configurations
-                    NSData  *sdata     = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://aihuishou.com%@", href]]];
-                    TFHpple *sdoc      = [[TFHpple alloc] initWithHTMLData:sdata];
-                    NSArray *selements = [sdoc searchWithXPathQuery:@"//div[@class='right']/ul/li/div"];
-                    for (int k = 0; k < [selements count]; k++) {
-                        TFHppleElement *e = [selements objectAtIndex:k];
-                        if (k == 0) {
-                            [log appendFormat:@"%d;", 100000 - [[e text] intValue]];
-                        } else {
-                            [log appendFormat:@"%@;", [e text]];
-                        }
-                        
-                    }
-                    [log appendString:@"\n"];
-                }
-            }
-        }
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
